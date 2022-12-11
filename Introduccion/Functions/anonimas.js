9943e7f49d102ad283ec08fb9f5d1325ae58bba0
CarlosFloresCS <carlos.flores.p@utec.edu.pe>
2022-12-10 23:29:12 -0500

    a

---
+++ Introduccion/Functions/anonimas.js
@@ -0,0 +1,19 @@
+// Clasica
+(function () {
+  console.log("version clasica");
+})();
+
+// Crockford
+((function () {
+  console.log("version crockford");
+})());
+
+// Unaria
++function () {
+  console.log("version unaria");
+}();
+
+// Facebook
+!function () {
+ console.log("version facebook"); 
+}();
